cc7cbc77bd26ef035df81ac4a3a42822bc36abf4
ckpark <1421088411@qq.com>
2013-08-09 08:04:49 +0800

    Create

---
+++ TestGit/src/com/zhq/HelloWord.java
@@ -0,0 +1,13 @@
+package com.zhq;
+
+public class HelloWord {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.print("hello word");
+	}
+
+}
